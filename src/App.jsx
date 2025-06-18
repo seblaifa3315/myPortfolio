9179f1cca77b5fc7e81a4ee20f771dceb9cc3de3
Seb <seblaifa@hotmail.com>
2025-06-18 16:03:53 -0700

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Navbar from "./components/sections/Navbar";
+
+export default function App() {
+
+    return (
+        <div className="container">
+            <Navbar />
+
+        </div>
+    );
+}
